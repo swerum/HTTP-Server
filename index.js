cfc301bc51399cf594d19c437aaa4f6d6b47f2be
swerum <sarah.elisabeth.werum@gmail.com>
2024-10-01 14:42:55 +0200

    Ch4: promise-based echo server

--- index.js
+++ index.js
@@ -1,26 +1,171 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// const net = require('node:net');
 var net = require("net");
-function onNewConnection(socket) {
-    console.log('new connection', socket.remoteAddress, socket.remotePort);
-    /** Set socket events (events that happen when the listening socket is open) */
+/** soInit sets up what happens to our promise in various events
+ * on data: resolve read with data. reset reader since the promise is resolved
+ * on end: resolve with empty data buffer
+ * on error: reject promise
+*/
+function soInit(socket) {
+    var conn = {
+        socket: socket, err: null, ended: false, reader: null,
+    };
     socket.on('data', function (data) {
-        console.log("data received: ", data.toString());
-        socket.write(data);
-        if (data.includes('q')) {
-            console.log("Ending because of q");
-            socket.end();
-        }
+        console.assert(conn.reader);
+        conn.socket.pause();
+        conn.reader.resolve(data);
+        conn.reader = null;
     });
     socket.on('end', function () {
-        console.log("End");
-        socket.destroy();
+        // this also fulfills the current read.
+        conn.ended = true;
+        if (conn.reader) {
+            conn.reader.resolve(Buffer.from('')); // EOF is signaled by empty buffer
+            conn.reader = null;
+        }
+    });
+    socket.on('error', function (err) {
+        // errors are also delivered to the current read.
+        conn.err = err;
+        if (conn.reader) {
+            conn.reader.reject(err);
+            conn.reader = null;
+        }
+    });
+    return conn;
+}
+/** soRead creates the promise. if the connection is already interrupted, reject/resolve with empty buffer
+ * Otherwise, store resolve and reject in connection object
+ */
+function soRead(conn) {
+    console.assert(!conn.reader); // no concurrent calls
+    return new Promise(function (resolve, reject) {
+        // if the connection is not readable, complete the promise now.
+        if (conn.err) {
+            reject(conn.err);
+            return;
+        }
+        if (conn.ended) {
+            resolve(Buffer.from('')); // EOF
+            return;
+        }
+        // save the promise callbacks
+        conn.reader = { resolve: resolve, reject: reject };
+        // and resume the 'data' event to fulfill the promise later.
+        conn.socket.resume();
+    });
+}
+/** check for connection error. create promise for writing.  */
+function soWrite(conn, data) {
+    console.assert(data.length > 0);
+    return new Promise(function (resolve, reject) {
+        if (conn.err) {
+            reject(conn.err);
+            return;
+        }
+        conn.socket.write(data, function (err) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve();
+            }
+        });
+    });
+}
+/** while true, read and the write back the data. if empty data, stop */
+function serveClient(socket) {
+    return __awaiter(this, void 0, void 0, function () {
+        var conn, data;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    conn = soInit(socket);
+                    _a.label = 1;
+                case 1:
+                    if (!true) return [3 /*break*/, 4];
+                    return [4 /*yield*/, soRead(conn)];
+                case 2:
+                    data = _a.sent();
+                    if (data.length === 0) {
+                        console.log('end connection');
+                        return [3 /*break*/, 4];
+                    }
+                    console.log('data', data);
+                    return [4 /*yield*/, soWrite(conn, data)];
+                case 3:
+                    _a.sent();
+                    return [3 /*break*/, 1];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+/** serve client until it stops, then destroy socket */
+function onNewConnection(socket) {
+    return __awaiter(this, void 0, void 0, function () {
+        var exc_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    console.log('new connection', socket.remoteAddress, socket.remotePort);
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 3, 4, 5]);
+                    return [4 /*yield*/, serveClient(socket)];
+                case 2:
+                    _a.sent();
+                    return [3 /*break*/, 5];
+                case 3:
+                    exc_1 = _a.sent();
+                    console.error('exception:', exc_1);
+                    return [3 /*break*/, 5];
+                case 4:
+                    socket.destroy();
+                    return [7 /*endfinally*/];
+                case 5: return [2 /*return*/];
+            }
+        });
     });
 }
 //Create Server
 var server = net.createServer();
-// Set events
 server.on('connection', onNewConnection);
 server.on('error', function (err) { throw err; });
 server.listen({ host: '127.0.0.1', port: 1234 });
--- index.ts
+++ index.ts
@@ -1,28 +1,127 @@
+import { rejects } from "assert";
+import { assert } from "console";
 import * as net from "net";
+import { resolve } from "path";
 
+type TCPConn = {
+    // the JS socket object
+    socket: net.Socket;
+    // from the 'error' event
+    err: null|Error;
+    // EOF, from the 'end' event
+    ended: boolean;
+    // the callbacks of the promise of the current read
+    reader: null|{
+        resolve: (value: Buffer) => void,
+        reject: (reason: Error) => void,
+    };
+};
 
-function onNewConnection(socket: net.Socket): void {
-    console.log('new connection', socket.remoteAddress, socket.remotePort);
-    /** Set socket events (events that happen when the listening socket is open) */
-    socket.on('data', (data : Buffer) => {
-        console.log("data received: ", data.toString());
-        socket.write(data);
-
-        if (data.includes('q')) {
-            console.log("Ending because of q");
-            socket.end();
-        }
+/** soInit sets up what happens to our promise in various events 
+ * on data: resolve read with data. reset reader since the promise is resolved
+ * on end: resolve with empty data buffer 
+ * on error: reject promise
+*/
+function soInit(socket: net.Socket): TCPConn {
+    const conn: TCPConn = {
+        socket: socket, err: null, ended: false, reader: null,
+    };
+    socket.on('data', (data: Buffer) => {
+        console.assert(conn.reader);
+        conn.socket.pause();
+        conn.reader!.resolve(data);
+        conn.reader = null;
     });
     socket.on('end', () => {
-        console.log("End");
-    })
+        // this also fulfills the current read.
+        conn.ended = true;
+        if (conn.reader) {
+            conn.reader.resolve(Buffer.from(''));   // EOF is signaled by empty buffer
+            conn.reader = null;
+        }
+    });
+    socket.on('error', (err: Error) => {
+        // errors are also delivered to the current read.
+        conn.err = err;
+        if (conn.reader) {
+            conn.reader.reject(err);
+            conn.reader = null;
+        }
+    });
+    return conn;
+}
+
+/** soRead creates the promise. if the connection is already interrupted, reject/resolve with empty buffer
+ * Otherwise, store resolve and reject in connection object
+ */
+function soRead(conn: TCPConn): Promise<Buffer> {
+    console.assert(!conn.reader);   // no concurrent calls
+    return new Promise((resolve, reject) => {
+        // if the connection is not readable, complete the promise now.
+        if (conn.err) {
+            reject(conn.err);
+            return;
+        }
+        if (conn.ended) {
+            resolve(Buffer.from(''));   // EOF
+            return;
+        }
+
+        // save the promise callbacks
+        conn.reader = {resolve: resolve, reject: reject};
+        // and resume the 'data' event to fulfill the promise later.
+        conn.socket.resume();
+    });
+}
+
+/** check for connection error. create promise for writing.  */
+function soWrite(conn: TCPConn, data: Buffer): Promise<void> {
+    console.assert(data.length > 0);
+    return new Promise((resolve, reject) => {
+        if (conn.err) {
+            reject(conn.err);
+            return;
+        }
+
+        conn.socket.write(data, (err?: Error) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+/** while true, read and the write back the data. if empty data, stop */
+async function serveClient(socket: net.Socket): Promise<void> {
+    const conn: TCPConn = soInit(socket);
+    while (true) {
+        const data = await soRead(conn);
+        if (data.length === 0) {
+            console.log('end connection');
+            break;
+        }
+
+        console.log('data', data);
+        await soWrite(conn, data);
+    }
+}
+
+/** serve client until it stops, then destroy socket */
+async function onNewConnection(socket: net.Socket): Promise<void> {
+    console.log('new connection', socket.remoteAddress, socket.remotePort);
+    try {
+        await serveClient(socket);
+    } catch (exc) {
+        console.error('exception:', exc);
+    } finally {
+        socket.destroy();
+    }
 }
 
 //Create Server
 let server = net.createServer();
-
-// Set events
 server.on('connection', onNewConnection);
 server.on('error', (err: Error) => { throw err; });
-
 server.listen({host: '127.0.0.1', port: 1234});
